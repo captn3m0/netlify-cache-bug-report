38d63a6db56be8780c27b43dd9488e006c16e4ea
Nemo <commits@captnemo.in>
2025-07-25 11:09:51 +0530

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM node:22.17.1-slim
 WORKDIR /code
 COPY package-lock.json package.json /code
-RUN npm ci && npm install -g netlify-cli@22.3.0
+RUN npm install -g netlify-cli@22.3.0 deno@1.46.0 && npm ci
+COPY netlify.toml /code
+COPY netlify /code/netlify
+CMD netlify dev --debug
 EXPOSE 8888
-CMD netlify dev
--- netlify/edge-functions/fetch-test/fetch-test.js
+++ netlify/edge-functions/fetch-test/fetch-test.js
@@ -2,6 +2,10 @@ import { fetchWithCache } from '@netlify/cache'
 export default async (request, context) => {
 
   let url = `https://example.com`
+  console.log("Current Deno version", Deno.version.deno);
+
+  console.log("Current TypeScript version", Deno.version.typescript);
+  console.log("Current V8 version", Deno.version.v8);
   const res = await fetchWithCache(url)
 
   if (!res.ok) {
